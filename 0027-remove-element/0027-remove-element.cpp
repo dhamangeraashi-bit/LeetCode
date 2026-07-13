d75739eccee803785c5490d93b691b85a6bb7348
Raashi Dhamange <dhamangeraashi@gmail.com>
2026-07-13 22:15:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (84.77%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j=0;int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==val){}
+            else {nums[j]=nums[i]; j++;}
+        }
+        return j;
+    }
+};
