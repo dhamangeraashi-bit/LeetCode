747f6c2788bcedbd45fc1122d5e1eed8fe2bba41
Raashi Dhamange <dhamangeraashi@gmail.com>
2026-07-16 00:45:02 +0530

    Time: 3 ms (16.38%), Space: 9.4 MB (18.59%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> mp;
+        for(char c:s)
+            mp[c]++;
+        int ans=0;
+        bool odd=false;
+
+        for(auto x:mp)
+        {
+            if(x.second%2==0)
+                ans+=x.second;
+            else{
+                ans+=x.second-1;
+                odd=true;
+            }
+        }
+        if(odd)
+             ans++;
+        return ans;
+    }
+};
