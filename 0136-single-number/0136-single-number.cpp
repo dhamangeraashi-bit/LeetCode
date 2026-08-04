b25b34e0f0ca530a28d3e6ae9b822729834da628
Raashi Dhamange <dhamangeraashi@gmail.com>
2026-08-04 23:10:55 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.33%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xoro=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            xoro=xoro^nums[i];
+        }
+        return xoro;
+    }
+};
